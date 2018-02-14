818436c5f33ee010ab6893b4ae51d7079dd8fdd6
myqijin <myqijin@foxmail.com>
2018-02-14 21:01:36 +0800

    primarily finished map

--- 51job/map_visualization.py
+++ 51job/map_visualization.py
@@ -3,92 +3,59 @@
 
 
 import csv
-import json
-import requests
-import urllib
 import random
 import time
+import pandas as pd
+import matplotlib.pyplot as plt
+from mpl_toolkits.basemap import Basemap
+from matplotlib.patches import Polygon
 
 
-def get_random_header():
-    list = [
-        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/22.0.1207.1 Safari/537.1",
-        "Mozilla/5.0 (X11; CrOS i686 2268.111.0) AppleWebKit/536.11 (KHTML, like Gecko) Chrome/20.0.1132.57 Safari/536.11",
-        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.6 (KHTML, like Gecko) Chrome/20.0.1092.0 Safari/536.6",
-        "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.6 (KHTML, like Gecko) Chrome/20.0.1090.0 Safari/536.6",
-        "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/19.77.34.5 Safari/537.1",
-        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.9 Safari/536.5",
-        "Mozilla/5.0 (Windows NT 6.0) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.36 Safari/536.5",
-        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3",
-        "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3",
-        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_0) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3",
-        "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1062.0 Safari/536.3",
-        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1062.0 Safari/536.3",
-        "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",
-        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",
-        "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",
-        "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.0 Safari/536.3",
-        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/535.24 (KHTML, like Gecko) Chrome/19.0.1055.1 Safari/535.24",
-        "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/535.24 (KHTML, like Gecko) Chrome/19.0.1055.1 Safari/535.24"
-    ]
-    headers = {
-        'User-Agent': random.choice(list),
-    }
-    return headers
-
-
-def opencsv(filename):
+def pdopencsv(filename):
     try:
-        with open(csv_file, 'r+', encoding='utf-8') as f:
-            reader = csv.reader(f)
-            datas = [data for data in reader]
-            return datas
+        datas = pd.read_csv(filename, encoding='gbk', header=0)
     except FileNotFoundError:
         print("Sorry, file cannot be found!")
     except PermissionError:
         print("File access denied!")
-
-
-def get_coordinate(url, param):
-    headers = get_random_header()
-    timeout = random.choice(range(80, 180))
-    webrequest = requests.get(url, headers=headers,
-                              params=param, timeout=timeout)
-    print(webrequest.status_code)
-    get_json = webrequest.json()
-    try:
-        lng = float(get_json["result"]["location"]["lng"])
-        lat = float(get_json["result"]["location"]["lat"])
-    except:
-        return None, None
     else:
-        return lng, lat
+        return datas
 
 
-def rew2csv(file_name, datas):
-    """将数据写入csv文件"""
-    with open(file_name, 'w', newline='', encoding='utf-8_sig') as csv_f:
-        writer = csv.writer(csv_f)
-        for data in datas:
-            writer.writerow(data)
+def basic_sh_map(ax=None, lllat=30.7, urlat=31.7, lllon=121., urlon=122.):
+    m = Basemap(ax=ax, projection='stere', lon_0=(urlon + lllon) / 2, lat_0=(urlat + lllat) / 2,
+                llcrnrlat=lllat, urcrnrlat=urlat, llcrnrlon=lllon, urcrnrlon=urlon, resolution='f')
+    m.drawcoastlines()
+    shp_info = m.readshapefile(
+        r"C:\Users\myqijin\workspace\my_scrapy\src\51job\CHN_adm_shp\CHN_adm3", 'states', drawbounds=True)
+    for info, shp in zip(m.states_info, m.states):
+        proid = info["NAME_2"]
+        if proid == "Shanghai":
+            poly = Polygon(shp, facecolor='w', edgecolor='b', lw=0.2)
+    return m, poly
 
 
 if __name__ == '__main__':
     """主函数"""
-
+    # 计时开始
+    start_time = time.clock()
     # 读取csv文件
     csv_file = "51job_test.csv"
-    datas = opencsv(csv_file)
-    locs = [column[8] for column in datas]
-    # 我的key
-    my_token = "zYSpyRGfEbDkqV1RiK12tY7HjQltDELj"
-    for i in range(0, len(locs)):
-        param = {'address': locs[i], 'output': 'json', 'ak': my_token}
-        url = "http://api.map.baidu.com/geocoder/v2/"
-        lng, lat = get_coordinate(url, param)
-        datas[i].append(lng)
-        datas[i].append(lat)
-        if i % 100 == 0:
-            time.sleep(3)
-    rew2csv('51job_test.csv', datas)
+    datas = pdopencsv(csv_file)
+    position = pd.DataFrame(datas, columns=["company", "lng", "lat"])
+    position = position.dropna(axis=0, thresh=2)
+    fig = plt.figure(figsize=(10, 8))
+    ax = fig.add_subplot(111)
+    map, ploy = basic_sh_map(ax)
+    ax.add_patch(ploy)
+    xi = position['lng'].tolist()
+    yi = position['lat'].tolist()
+    x, y = map(xi, yi)
+    map.scatter(x, y, s=1, c='r')
+    ax.set_title("Position Distribution")
+    plt.show()
+
+    # 计时结束
+    end_time = time.clock()
+    print("Consuming time: %.3f s." % (end_time - start_time))
     print("Mission Complete!")
